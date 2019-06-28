3d66d4c7148ecbe11ee687391c9990db9c8ecadd
YaninaContreras <yanicontreras.23@gmail.com>
2019-06-28 16:42:10 -0500

    terminando el screen 1 login

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
 /* Manejo del DOM */
+
+/* llamando IDs*/
+
+const inputcorreo = document.getElementById("inputcorreo")
+const inputcontraseña = document.getElementById("inputcontraseña")
+const ingresa = document.getElementById("ingresa")
+const error = document.getElementById("error")
+const segundapantalla = document.getElementById("segundapantalla")
+ingresa.addEventListener("click", () => {
+    if (inputcorreo.value === "LABORATORIA" && inputcontraseña.value === "LABORATORIA") {
+        segundapantalla.classList.toggle("show")
+        login.classList.toggle("hide")
+    }
+    else {
+        error.innerHTML = "contraseña incorrecta"
+    }
+})
