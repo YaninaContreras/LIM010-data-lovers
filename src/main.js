a1b507c6378e219da46678de936b96d21894fe83
Elvia <elvia-love90@hotmail.com>
2019-07-01 08:34:45 -0500

    html initio

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-/* Manejo del DOM */
+const email = document.getElementById("user")
+const psw = document.getElementById("psw")
+const button = document.getElementById("next")
+const fail =document.getElementById ("fail")
+const firstsreen = document.getElementById("login")
+const secondsreen =document.getElementById("statements")
+button.addEventListener("click", () => {
+  if (email.value === "LABORATORIA" && psw.value === "LABORATORIA") {
+    secondsreen.classList.toggle("show")  
+    firstsreen.classList.toggle("hide")
+  }
+  else {
+    fail.innerHTML="contraseña incorrecta"
+  }
+})
