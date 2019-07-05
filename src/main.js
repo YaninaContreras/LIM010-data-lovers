2dd84c4bb4f3f41e124e6aa61be2082d80d464a6
YaninaContreras <yanicontreras.23@gmail.com>
2019-07-04 20:15:51 -0500

    agregandole HTML y css a la segunda pantalla

--- src/main.js
+++ src/main.js
@@ -13,17 +13,3 @@ ingresa.addEventListener('click', () => {
     error.innerHTML = 'contraseña incorrecta'
   };
 });
-
-const worldbank = WORLDBANK;
-const mostrardata = (data) => {
-  let mostrar = "";
-  for (let i = 0; i < data; i++) {
-    let llamado.push = (´
-    <div class="first"> 
-     <input type="text">${element.data}</input>
-    </div>
-    mostrar+=llamado
-    )
-  }
-  return mostrar 
-} 
