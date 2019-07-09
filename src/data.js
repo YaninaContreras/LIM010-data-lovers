5a8ca3d05bd21df5b849d387743c0c50988cc31f
Elvia <elvia-love90@hotmail.com>
2019-07-08 19:25:05 -0500

    creando estructura html pantalla2

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-<<<<<<< HEAD
+
 const array = (dataparametro) => {
   const wbdata = [];
   for (let i = 0; i < dataparametro.length; i++) {
@@ -20,59 +20,39 @@ const demografico = (element) => {
 };
 
 const laboral = (element) => {
-  let labocode = element.indicatorCode;
+  let labocode = element.indicatorCode
   let initlab = labocode.slice(0, 6);
   return initlab === 'SE.PRM';
 };
 
 const demografilter = (data) => {
-  let arrfilter= data.filter (demografico);
+  let arrfilter= data.filter (demografico)
   return arrfilter;
 };
 const labofilter = (data) => {
   let arrfilter = data.filter (laboral);
   return arrfilter;
 };
-window.worldbank = {
-  demografico : demografico,
-  laboral : laboral,
-  demografilter:demografilter,
-  labofilter : labofilter,
-};
-=======
-const copiardata = (Dataparametro) => {
-  const datawb = [ ]
-  for (let i = 0; i < 1399; i++) {
-    datawb.push(Object.assign({}, Dataparametro[i]))
+
+const indicatorsperu = () => {
+  let demoperu = []
+  let labperu = []
+
+  for (let i = 0; i < 26; i++) {
+    demoperu.push(filterdemogperu[i].indicatorName)
+    labperu.push(filterlabperu[i].indicatorName)
   }
-  return datawb
+  return {
+    demoperu: demoperu,
+    labperu: labperu
+
+  }
 }
-const tenerdata = (arraypais, index) => {
-  const data = arraypais[index]
-  return data
-}
-const demográfico = (element) => {
-  let codedemográfico = element.indicatorCode
-  let initdemografico = codedemográfico.slice(0, 6)
-  return initdemografico === 'UNT.POP.SP'
-}
-const laboral = (element) => {
-  let laboralcode = element.indicatorCode
-  let initlaboral = laboralcode.slice(0, 6)
-  return initlaboral === 'TLF.NE'
-}
-// const demográficofilter = (data) => {
-//   let filter = data.filter(demográfico)
-//   return filter
-// }
-// const laboralfilter = (data) => {
-//   let filter = data.filter(laboral)
-//   return filter
-// }
 window.worldbank = {
-  demográfico,
-  laboral,
-  // demográficofilter,
-  // laboralfilter
+  demografico: demografico,
+  laboral: laboral,
+  demografilter: demografilter,
+  labofilter: labofilter,
+  indicatorsperu: indicatorsperu
+
 }
->>>>>>> c295f4b8cbe84c0dc3475d63bd57dc0444bc0f04
