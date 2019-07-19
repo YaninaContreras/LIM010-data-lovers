0c624427120ee497dbe3668963bbe61126908cff
Elvia <elvia-love90@hotmail.com>
2019-07-19 10:30:41 -0500

    Usando un array de objetos para la tabla

--- src/data.js
+++ src/data.js
@@ -7,32 +7,14 @@ const obtenerIndicadoresPorPais = (obj, string) => {
   const arrIndicadores = obj[string].indicators;
   return arrIndicadores;
 };
-
-//Ordenar data ascendente y descendente//
-
-const orderYears = (datadeobjetos) => { // {}
-  const arraydeindicadores = Object.entries(datadeobjetos);
-  console.log(arraydeindicadores);
-  return arraydeindicadores.sort((antes, despues) => {
-    return antes[1] - despues[1];
-  })
+// 
+const obtenerObjetoData = (WORLDBANK, strings, indicador) => {
+  const objetoData = WORLDBANK[strings].indicators[indicador].data;
+  return objetoData;
 };
-const filtroaños = (inputdesde, inputhasta, años) => {
-  const key = Object.keys(años)
-  const value = Object.values(años)
-  let rangoanios = [ ]
-  for (let j = 0; j < key.length; j++) {
-    if (key[j] >= inputdesde.value && key[j] <= inputhasta.value) {
-      rangoanios.push([key[j], value[j]])
-    }
-  }
-  return rangoanios
-}
-//Asignando variables globales//
 
 window.worldbank = {
+  obtenerObjetoData: obtenerObjetoData,
   formarArrayDePaises: formarArrayDePaises,
   obtenerIndicadoresPorPais: obtenerIndicadoresPorPais,
-  filtroaños: filtroaños,
-  orderYears: orderYears
 }
